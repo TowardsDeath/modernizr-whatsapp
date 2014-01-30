1cf38de984896724b1676cb62c1f9514a232c674
Do Normal <robert@versolamorte.nl>
2014-01-30 13:39:53 +0100

    Initial commit for project page.

--- whatsapp.js
+++
@@ -1,4 +0,0 @@
-Modernizr.addTest( 'whatsapp', function () {
-    var isSupportedOs = /android|iphone|blackberry([0-9]+\/(4\.[6-9]|[5-9])|.+Version\/[5-9])|series(40|60)|windows phone( os)? (7\.5|8)/i.test( navigator.userAgent || navigator.vendor );
-    return isSupportedOs;
-} );
